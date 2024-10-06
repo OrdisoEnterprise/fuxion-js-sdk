de5f578a8f0cb2905f6a8a693038e190bdcef3b9
asanchez <tito97_sp@hotmail.com>
2024-10-06 23:17:18 +0200

    bug: querystring.stringfy issue

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -439,7 +439,7 @@ class ApiClient {
         }
 
         if (contentType === 'application/x-www-form-urlencoded') {
-            request.send(JSON.stringify(this.normalizeParams(formParams)));
+            request.send(new URLSearchParams(this.normalizeParams(formParams)).toString());
         } else if (contentType == 'multipart/form-data') {
             var _formParams = this.normalizeParams(formParams);
             for (var key in _formParams) {
